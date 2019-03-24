fae9195f1b984ac05eeea4c6a0ad06ef2a9bedcc
21clshao96 <32548119+21clshao96@users.noreply.github.com>
2019-03-24 15:42:57 -0400

    Add files via upload
    list features

---
+++ ProjectIgniteGroceries-checklist/CoreMLDemo/Checklist.swift
@@ -0,0 +1,15 @@
+//
+//  Checklist.swift
+//  CoreMLDemo
+//
+//  Created by Qianou Ma on 3/24/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class Checklist {
+    var name = ""
+    var important = false
+    
+}
